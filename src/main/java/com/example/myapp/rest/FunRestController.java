cc9391f6a56237afca9880f3f9822432e1416952
jrexiti <jay.rexiti@gmail.com>
2024-08-29 19:41:15 -0500

    custom properties / more security on endpoints

--- src/main/java/com/example/myapp/rest/FunRestController.java
+++ src/main/java/com/example/myapp/rest/FunRestController.java
@@ -1,11 +1,17 @@
 package com.example.myapp.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class FunRestController {
+
+    @Value("${accountHolder.name}")
+    private String userName;
+
+    @Value("${accountHolder.age}")
+    private String age;
 
     // expose a "/" endpoint that returns "Hello World"
 
@@ -16,7 +22,7 @@ public class FunRestController {
 
     @GetMapping("/account")
     public String getAccount() {
-        return "Welcome to your Account Page";
+        return "Welcome to your Account Page: " + userName + " " + "age" + ": " + age;
     }
 
     @GetMapping("/contactus")
